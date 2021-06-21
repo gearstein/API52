8402249b00eba6413f2c6c671dd773ac55b57df2
gearstein <48464442+gearstein@users.noreply.github.com>
2021-06-21 20:26:08 +0700

    Create API

---
+++ API52/API52/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API52.Models
+{
+    [Table("tb_M_Employee")]
+    public class Employee
+    {
+        public enum GenderRole
+        {
+            Pria = 1, Wanita = 2
+        }
+        [Key]
+        public string NIK { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public GenderRole Gender { get; set; }
+        public string Email { get; set; }
+        public int Salary { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+        [JsonIgnore]
+        public virtual Account Account{ get; set; }
+    }
+}
---
+++ API52/API52/Repository/GeneralRepository.cs
@@ -0,0 +1,56 @@
+﻿using API52.Context;
+using API52.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API52.Repository
+{
+    public class GeneralRepository<Context, Entity, Key> : IRepository<Entity, Key>
+        where Context : MyContext
+        where Entity : class
+    {
+        private readonly MyContext myContext;
+        private readonly DbSet<Entity> entities;
+        public GeneralRepository(MyContext myContext)
+        {
+            this.myContext = myContext;
+            entities = myContext.Set<Entity>();
+        }
+        public int Delete(Key key)
+        {
+            var find =  entities.Find(key);
+            if(find != null)
+            {
+                entities.Remove(find);
+            }
+            return myContext.SaveChanges();
+        }
+
+        public IEnumerable<Entity> Get()
+        {
+            return entities.ToList();
+        }
+
+        public Entity Get(Key key)
+        {
+            return entities.Find(key);
+        }
+
+        public int Insert(Entity e)
+        {
+            entities.Add(e);
+            var insert = myContext.SaveChanges();
+            return insert;
+        }
+
+        public int Update(Entity e, Key key)
+        {
+            myContext.Entry(e).State = EntityState.Modified;
+            myContext.Update(e);
+            return myContext.SaveChanges();
+        }
+    }
+}
